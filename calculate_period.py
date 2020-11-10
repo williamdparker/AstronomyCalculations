6c0110ba49e18f550519e51ba335fa42a43e5274
William <baiweilian@gmail.com>
2020-11-09 20:32:20 -0600

    Created try-except block for sidereal period calculation

--- calculate_period.py
+++ calculate_period.py
@@ -2,9 +2,14 @@ import numpy as np
 
 
 def calculate_sidereal_period(inferior_period, superior_period):
-    if inferior_period > 0. and superior_period > 0.:
-        return 1./((1./inferior_period) - (1./superior_period))
-    else:
-        return 0.
+    try:
+        sidereal_period = 1./((1./inferior_period) - (1./superior_period))
+    except ZeroDivisionError:
+        print("Error: zero is an invalid period value")
+        sidereal_period = 0.
+    finally:
+        return sidereal_period
+
+
 
 
