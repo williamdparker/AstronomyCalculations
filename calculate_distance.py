ca60b1ff381ed699887c768b67f21a8820c45868
Riley <riles1045@gmaIL.com>
2020-11-15 13:05:19 -0600

    Initial Commit - 11/15/20

--- calculate_distance.py
+++ calculate_distance.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 
-def calculate_superior_distance(quadrature_time, planet_sidereal_period,
+def calculate_superior_distance(quadrature_time=87.5, planet_sidereal_period=4328.9,
                                 earth_sidereal_period=365.256, earth_sun_distance=1.):
     # define alpha
-
+    alpha = (quadrature_time * (2 * np.pi / earth_sidereal_period))
     # define beta
+    beta = (quadrature_time * (2 * np.pi) / planet_sidereal_period)
 
     return earth_sun_distance / np.cos(alpha - beta)
 
 
-def calculate_inferior_distance(greatest_elongation, earth_sun_distance=1.):
+def calculate_inferior_distance(greatest_elongation=0.489, earth_sun_distance=1.):
     return earth_sun_distance * np.sin(greatest_elongation)
