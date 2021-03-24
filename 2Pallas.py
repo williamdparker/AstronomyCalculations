de8f0afb259a8ec78e85718325d0618ac5d01cea
riley10445 <riles1045@gmail.com>
2021-03-24 16:44:31 -0500

    Added file for 2Pallas and imported original data

---
+++ 2Pallas.py
@@ -0,0 +1,7 @@
+from astropy.time import Time
+
+# Values taken from Chapter 13.6 of Celestial Mechanics (Tatum)
+times = ('2002-06-10', '2002-07-15', '2002-07-25'),
+# Values in radians
+right_ascension = (5.5649, 5.5521, 5.5222),
+declination = (0.2833, 0.2803, 0.2690),
