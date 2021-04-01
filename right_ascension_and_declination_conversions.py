79f7f9e3e43ee68c61801a46b887d465dfcf2a33
williamparker <baiweilian@gmail.com>
2021-04-01 06:05:02 -0500

    Fixed dangling commas and functionalized conversions and check

--- right_ascension_and_declination_conversions.py
+++ right_ascension_and_declination_conversions.py
@@ -2,22 +2,47 @@ import numpy as np
 
 # Values taken at 8:15 p.m. 3-31-2021
 right_ascension = '00h 41m 22s'
-declination = '+04° 26m 56m'
+declination = '+04° 26m 56s'
 
-# α = right ascension, δ = declination in radians
-δ = 0.0120
-α = 0.0776
 
-x = np.cos(δ) * np.cos(α),
-y = np.cos(δ) * np.sin(α),
-z = np.sin(δ),
+def right_ascension_hms_string_to_radians_float(ra):
+    """Given input RA in format ##h ##m ##s return RA in decimal radians"""
+    # Note: we should probably adapt to ##.##s form as well
+    hours = float(ra[:1])
+    minutes = float(ra[4:5])/60.                                   # 60 m in 1 h
+    seconds = float(ra[8:9])/3600.                                 # 3600 s in 1 h
+    right_ascension_float = (hours + minutes + seconds)*np.pi/12.  # 2π rad in 24 h
+    return right_ascension_float
 
-l = x,
-m = y,
-n = z,
 
-test = l**2 + m**2 + n**2
+def declination_dms_string_to_radians_float(dec):
+    """Given input declination in format +##d ##m ##s return declination in decimal radians"""
+    return declination_float
 
-print(test)
+
+def declination_dms_string_to_radians_float(δ, α):
+    """Given input declination and RA in radians, return [l,m,n] angular component of geocentric equatorial
+    coordinates """
+    l = np.cos(δ) * np.cos(α)
+    m = np.cos(δ) * np.sin(α)
+    n = np.sin(δ)
+    geocentric_coordinates_angular_part = [l, m, n]
+    return geocentric_coordinates_angular_part
+
+
+def test_geocentric_coordinates_angular_part(coordinates):
+    sum_of_squares = 0
+    for index in len(coordinates):
+        sum_of_squares += coordinates[index]**2
+    if sum_of_squares == 1.:
+        return True
+    else:
+        print(f"l^2 + m^2 + n^2 = {sum_of_squares} != 1")
+        return False
+
+
+print(right_ascension_hms_string_to_radians_float(right_ascension))
+
 
 # TypeError: unsupported operand type(s) for ** or pow(): 'tuple' and 'int'
+## solution: don't place commas at the ends of lines
